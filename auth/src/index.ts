b51eb3bcfc293fbb2d1bf55814bcf2edb53d8342
marianosegura <luismariano10del4@gmail.com>
2022-05-25 19:13:57 -0600

    Created test auth service and skaffold setup

---
+++ auth/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+
+const app = express();
+app.use(json());
+
+
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there!');
+});
+
+
+app.listen(3000, () => {
+  console.log('Auth service at port 3000...')
+});
